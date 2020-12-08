fe219842f5923abd8bec2c7a420188780a70c9b1
KeanuReeeves <71759633+KeanuReeeves@users.noreply.github.com>
2020-12-08 12:08:22 +0100

    Add project files.

---
+++ LatvanyossagokApplication/Form1.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LatvanyossagokApplication
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new MySqlConnection("Server=localhost; Database=latvanyossagokdb; Uid=root; Pwd=;");
+            conn.Open();
+
+            this.FormClosed += (sender, args) => {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            };
+        }
+        
+    }
+}
