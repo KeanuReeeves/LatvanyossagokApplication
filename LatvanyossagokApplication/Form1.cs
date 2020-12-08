c9b6b54db4f90ef8cc84e1fbf68acfab4bf82bb3
KeanuReeeves <71759633+KeanuReeeves@users.noreply.github.com>
2020-12-08 13:25:41 +0100

    database connect, create tables

--- LatvanyossagokApplication/Form1.cs
+++ LatvanyossagokApplication/Form1.cs
@@ -17,15 +17,50 @@ namespace LatvanyossagokApplication
         public Form1()
         {
             InitializeComponent();
-            conn = new MySqlConnection("Server=localhost; Database=latvanyossagokdb; Uid=root; Pwd=;");
-            conn.Open();
+            try
+            {
+                conn = new MySqlConnection("Server=localhost; Database=latvanyossagokdb; Uid=root; Pwd=;");
+                conn.Open();
 
-            this.FormClosed += (sender, args) => {
-                if (conn != null)
+                this.FormClosed += (sender, args) =>
                 {
-                    conn.Close();
-                }
-            };
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                };
+            }
+            catch (MySqlException)
+            {
+                MessageBox.Show("Hiba az adatbázis kapcsolódáskor!");
+                this.Close();
+            }
+            tablaCreate();
+        }
+        private void tablaCreate()
+        {
+            string sql = @"Create table IF NOT EXISTS varosok(
+                        id int NOT NULL AUTO_INCREMENT,
+                        nev varchar(52) NOT NULL UNIQUE,
+                        lakossag int NOT NULL,
+                        PRIMARY KEY (id)
+                        );";
+            var comm = this.conn.CreateCommand();
+            comm.CommandText = sql;
+            comm.ExecuteNonQuery();
+            sql = @"Create table IF NOT EXISTS latvanyossagok(
+                        id int NOT NULL AUTO_INCREMENT,
+                        nev varchar(52) NOT NULL,
+                        leiras text(152) NOT NULL,
+                        ar int NOT NULL,
+                        varos_id int NOT NULL,
+                        PRIMARY KEY (id),
+                        FOREIGN KEY (varos_id) REFERENCES varosok(id) 
+                        ON DELETE CASCADE ON UPDATE RESTRICT
+                        );";
+            comm = this.conn.CreateCommand();
+            comm.CommandText = sql;
+            comm.ExecuteNonQuery();
         }
         
     }
